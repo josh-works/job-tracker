8fb4be70530cc0d70f0ef1881ab9ce56ce6c4ef1
Josh Thompson <thompsonjoshd@gmail.com>
2017-04-01 11:52:37 -0600

    add no duplicate category test

--- spec/features/categories/user_creates_a_category_spec.rb
+++ spec/features/categories/user_creates_a_category_spec.rb
@@ -7,40 +7,21 @@ describe "User creates a new category" do
     fill_in "category[title]", with: "Web Developer"
     click_on "Create"
 
-    expect(current_path).to eq(category_path(Category.last))
+    expect(current_path).to eq(categories_path)
     expect(Category.count).to eq(1)
-    within(".title") do
+    within("li") do
       expect(page).to have_content("Web Developer")
     end
+  end
 
+  scenario "but user cannot create duplicate category" do
+    Category.create(title: "Web Developer")
+    expect(Category.count).to eq(1)
 
+    visit new_category_path
+    fill_in "category[title]", with: "Web Developer"
+    click_on "Create"
 
+    expect(Category.count).to eq(1)
   end
 end
-
-
-
-# describe "User creates a new category" do
-#   scenario "a user can create a new category" do
-#     visit new_category_path
-#
-#     fill_in("category[title]", with: "Education")
-#     click_on("Create")
-#
-#     expect(current_path).to eq(category_path(Category.last))
-#     expect(Category.count).to eq(1)
-#     within(".title") do
-#       expect(page).to have_content("Education")
-#     end
-#   end
-#
-#   scenario "a user cannot create duplicate categories" do
-#     Category.create(title: "Education")
-#     visit new_category_path
-#
-#     fill_in("category[title]", with: "Education")
-#     click_on("Create")
-#
-#     expect(Category.count).to eq(1)
-#   end
-# end
